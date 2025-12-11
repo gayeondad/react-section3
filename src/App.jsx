45ff654699b670fffa4cdcf295da960a06e154f8
gayeondaddy <gayeondaddy@gmail.com>
2025-12-12 05:54:50 +0900

    map

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 // import componentImage from './assets/components.png';
 import CoreConcept from './components/CoreConcept.jsx';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import TapButton from './components/TabButton.jsx';
 
 
-
 function App() {
-  const [selectedTab, setSelectedTab] = useState('Please select a tab');
+  const [selectedTab, setSelectedTab] = useState();
 
   function handelSelect(selectedButton) {
     setSelectedTab(selectedButton);
     console.log(`Tab "${selectedTab}" selected`);
+  }
+
+  let tabContent = <p>Please select a tab to see an example.</p>;
+  if (selectedTab) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTab].title}</h3>
+        <p>{EXAMPLES[selectedTab].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTab].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
   
   return (
@@ -22,25 +36,20 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              image={CORE_CONCEPTS[0].image}
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TapButton onSelect={() => handelSelect('Components')}>Components</TapButton>
-            <TapButton onSelect={() => handelSelect('JSX')}>JSX</TapButton>
-            <TapButton onSelect={() => handelSelect('State')}>State</TapButton>
-            <TapButton onSelect={() => handelSelect('Props')}>Props</TapButton>
+            <TapButton isSelected={selectedTab === 'components'} onSelect={() => handelSelect('components')}>Components</TapButton>
+            <TapButton isSelected={selectedTab === 'jsx'} onSelect={() => handelSelect('jsx')}>JSX</TapButton>
+            <TapButton isSelected={selectedTab === 'state'} onSelect={() => handelSelect('state')}>State</TapButton>
+            <TapButton isSelected={selectedTab === 'props'} onSelect={() => handelSelect('props')}>Props</TapButton>
           </menu>
-          {selectedTab}
+          {tabContent}
         </section>
       </main>
     </div>
